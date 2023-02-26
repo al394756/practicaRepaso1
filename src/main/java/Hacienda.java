3182c12aa1b70174f4aefcf97efaa50c0cab4775
David del Río <78787392+al394756@users.noreply.github.com>
2023-02-26 18:50:16 +0100

    todo guay

---
+++ src/main/java/Hacienda.java
@@ -0,0 +1,12 @@
+public class Hacienda {
+    public static void main(String[] args) {
+        //Existe una persona de nombre Javier Andrés Martín
+
+
+        //Tiene a su nombre 2 propiedades: una casa y una motocicleta
+
+        //Ha conseguido pagar su casa por lo que ya es suya
+
+        //Decide cambiar su preciosa motocicleta por un coche
+    }
+}
